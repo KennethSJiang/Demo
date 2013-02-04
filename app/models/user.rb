d46c241b8c4b83375a469386f5a05b0aa6d55f24
Kenneth Jiang <kenneth.s.jiang@gmail.com>
2013-02-03 20:03:32 -0500

    chapter 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
   has_many :microposts
+  
+  def initialize(attributes={})
+    @name = attribute[:name]
+    @email = attribute[:email]
+  end
+    
+   def formatted_email
+     "#{@name} <#{@email}>"
+   end
 end
+
