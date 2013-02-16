cf20ce4e3ef5a58655073f3ef16e6ba4840a1bcc
Kenneth Jiang <kenneth.s.jiang@gmail.com>
2013-02-16 15:03:16 -0500

    chapter 9

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,6 +15,9 @@ module SessionsHelper
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
   
+  def current_user?(user)
+    current_user == User
+  end
   #check if signed in
   def signed_in?
     !current_user.nil?
@@ -23,5 +26,14 @@ module SessionsHelper
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
+  end
+  
+  def store_location
+    session[:return_to] = request.url
+  end
+  
+  def redirect_back_or(default)
+    redirect_to session[:return_to] || default
+    session.delete(:return_to)
   end
 end
